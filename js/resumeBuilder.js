5c6083e988c59a793b09bb248895e9de20144059
Stephanie Picard <Stephanie Picard>
2016-10-26 13:29:46 -0400

    fix: restructered contacts to an object per the class solve

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,10 +4,12 @@ var bio = {
 	"name" : "Stephanie Picard",
 	"role" : "Web Developer",
 	"biopic" : "images/me.jpg",
-	"mobile" : "+1 859-537-6998",
-	"email" : "pcrdstphn@yahoo.com",
-	"github" : "github/pcrdstphn",
-	"location" : "Lake George, NY",
+	"contacts": {
+		"mobile" : "+1 859-537-6998",
+		"email" : "pcrdstphn@yahoo.com",
+		"github" : "github.com/pcrdstphn",
+		"location" : "Lake George, NY"
+	},
 	"skills" : ["Front End Web Dev", "Manufacturing Excellence", "Leadership"],
 	"welcomemsg": "Welcome to my online Resume"
 	};
@@ -16,12 +18,12 @@ var bio = {
 var formattedName=HTMLheaderName.replace("%data%", bio.name);
 var formattedRole=HTMLheaderRole.replace("%data%", bio.role);
 var formattedbioPic=HTMLbioPic.replace("%data%", bio.biopic);
-var formattedMobile=HTMLmobile.replace("%data%", bio.mobile);
-var formattedEmail=HTMLemail.replace("%data%", bio.email);
-var formattedGithub=HTMLgithub.replace("%data%", bio.github);
+var formattedMobile=HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail=HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub=HTMLgithub.replace("%data%", bio.contacts.github);
 var formattedSkills=HTMLskills.replace("%data%", bio.skills);
 var formattedWelcomeMsg=HTMLwelcomeMsg.replace("%data%", bio.welcomemsg);
-var formattedLocation=HTMLlocation.replace("%data%", bio.location);
+var formattedLocation=HTMLlocation.replace("%data%", bio.contacts.location);
 
 
 $("#header").prepend(formattedRole);
