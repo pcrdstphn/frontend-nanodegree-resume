765c6b8293e93df830362df867f939540bc9b12e
Stephanie Picard <Stephanie Picard>
2016-10-26 13:23:17 -0400

    docs:  add my picture image feat: initial javascript in re resumeBuilder file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,45 @@
+
+
+var bio = {
+	"name" : "Stephanie Picard",
+	"role" : "Web Developer",
+	"biopic" : "images/me.jpg",
+	"mobile" : "+1 859-537-6998",
+	"email" : "pcrdstphn@yahoo.com",
+	"github" : "github/pcrdstphn",
+	"location" : "Lake George, NY",
+	"skills" : ["Front End Web Dev", "Manufacturing Excellence", "Leadership"],
+	"welcomemsg": "Welcome to my online Resume"
+	};
+
+
+var formattedName=HTMLheaderName.replace("%data%", bio.name);
+var formattedRole=HTMLheaderRole.replace("%data%", bio.role);
+var formattedbioPic=HTMLbioPic.replace("%data%", bio.biopic);
+var formattedMobile=HTMLmobile.replace("%data%", bio.mobile);
+var formattedEmail=HTMLemail.replace("%data%", bio.email);
+var formattedGithub=HTMLgithub.replace("%data%", bio.github);
+var formattedSkills=HTMLskills.replace("%data%", bio.skills);
+var formattedWelcomeMsg=HTMLwelcomeMsg.replace("%data%", bio.welcomemsg);
+var formattedLocation=HTMLlocation.replace("%data%", bio.location);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedbioPic);
+$("#header").append(HTMLskillsStart);
+$("#skills-h3").append(formattedSkills);
+$("#header").append(formattedWelcomeMsg);
+$("#topContacts").append(formattedLocation);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+
+
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+	
+	"location":"Lake George, NY";
+	"welcome-message": "Welcome to my online Resume";
+	
+*/
